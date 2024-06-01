791d1b48398ce1df7dc7c1bc265d7c79337e02b7
kvitee <kvitee41@gmail.com>
2024-06-01 20:21:55 +0800

    Delete root (/) route

--- routes/router.js
+++ routes/router.js
@@ -4,15 +4,7 @@ import { userRouteHandler } from "./userRoutes/router.js";
 async function routeHandler(request, response) {
   response.setHeader("Content-Type", "application/json");
 
-  if (request.url === "/") {
-    response
-      .status(200)
-      .end(
-        JSON.stringify({
-          message: "Hello, World!"
-        })
-      );
-  } else if (request.url.startsWith("/users")) {
+  if (request.url.startsWith("/users")) {
     await userRouteHandler(request, response);
   } else {
     response

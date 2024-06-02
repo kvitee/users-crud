304cf1fa9f17a0017f2b85930c7a55a10b1939af
kvitee <kvitee41@gmail.com>
2024-06-02 18:47:47 +0800

    Rewrite userRouteHandler using switch statement

--- routes/userRoutes/router.js
+++ routes/userRoutes/router.js
@@ -6,16 +6,23 @@ import { RouteNotFoundError } from "../exceptions/RouteNotFound.js";
 
 
 async function userRouteHandler(request, response) {
-  if (request.method === "GET") {
-    if (request.url.match(/^\/users\/?$/)) {
-      await getUsers(request, response);
-    } else {
-      await getUser(request, response);
-    }
-  } else if (request.method === "POST") {
-    await createUser(request, response);
-  } else {
-    throw new RouteNotFoundError(request.url, request.method);
+  switch (request.method) {
+    case "GET":
+      if (request.url.match(/^\/users\/?$/)) {
+        await getUsers(request, response);
+      } else {
+        await getUser(request, response);
+      }
+
+      break;
+
+    case "POST":
+      await createUser(request, response);
+      break;
+
+    default:
+      throw new RouteNotFoundError(request.url, request.method);
+      break;
   }
 }
 

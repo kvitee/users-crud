2c12280065658e02cceb0e3b6db6d4bee751d700
kvitee <kvitee41@gmail.com>
2024-06-02 23:11:10 +0800

    Add RouteNotFound exception to createUser() function

--- routes/userRoutes/createUser.js
+++ routes/userRoutes/createUser.js
@@ -1,8 +1,16 @@
 import { storage } from "../../storage/arrayStorage.js";
 import { parseRequestBody } from "../../utils/requestBodyParser.js";
 
+import { RouteNotFoundError } from "../exceptions/RouteNotFound.js";
+
 
 async function createUser(request, response) {
+  const match = request.url.match(/^\/users\/?$/);
+
+  if (!match) {
+    throw new RouteNotFoundError(request.url, request.method);
+  }
+
   const body = await parseRequestBody(request);
 
   response

b092ab2b74c6b37ab00d72e288db91f380422965
kvitee <kvitee41@gmail.com>
2024-06-06 11:28:49 +0800

    Create custom pg type parsers module.
    Module contains function which sets parser for BigInt datatype.
    It converts all BigInt numbers to regular integer
    because the values in database will never exceed its limit.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import { createServer } from "node:http";
 import { routeHandler } from "./routes/router.js";
 
+import pg from "pg";
+
+import { setPgTypeParsers } from "./utils/pgTypeParsers.js";
+
 
 const server = createServer(routeHandler);
 
 server.listen(3000, "127.0.0.1", () => {
   console.log("Listening on 127.0.0.1:3000");
 });
+
+/* Setting custom Postgres type parsers. */
+setPgTypeParsers(pg);

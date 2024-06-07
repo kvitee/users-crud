87a5cc70a7d098743011669326adff7161086891
kvitee <kvitee41@gmail.com>
2024-06-07 21:09:53 +0800

    Add CRUD_PORT environment variable.

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ import { setPgTypeParsers } from "./utils/pgTypeParsers.js";
 
 const server = createServer(routeHandler);
 
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Listening on 127.0.0.1:3000");
+const port = process.env.CRUD_PORT || 3000;
+
+server.listen(port, "127.0.0.1", () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
 });
 
 /* Setting custom Postgres type parsers. */
